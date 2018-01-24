5d39a689b515b6bff165eb09c3b8a5f65f6a2615
Dave Stearns <dlsinfo@uw.edu>
2018-01-24 11:55:54 -0800

    more explanatory comments

--- functional/js/index.js
+++ functional/js/index.js
@@ -5,7 +5,7 @@
 
 //FILTERING
 /**
- * Returns true if rain === true
+ * Returns true if record.rain === true
  * @param {WeatherRecord} record 
  * @returns boolean
  */
@@ -13,8 +13,19 @@ function isRainy(record) {
     return record.rain === true;
 }
 
-//this is essentially what the .filter()
-//method on arrays does
+/**
+ * Arrays have a built-in .filter() method, but
+ * if you want to understand what that method does,
+ * read this code. This function does exactly what
+ * Array.filter() does. Note how the basic filtering
+ * algorithm is separated from the predicate function
+ * that tests each element in the array. We can provide
+ * different predicate functions depending on what sort
+ * of filtering we wnat to achieve.
+ * @param {Array} array 
+ * @param {function(*)} predicate 
+ * @returns {Array}
+ */
 function filter(array, predicate) {
     let output = [];
     for (let i = 0; i < array.length; i++) {
@@ -25,26 +36,64 @@ function filter(array, predicate) {
     return output;
 }
 
+/**
+ * Returns a new predicate function that calls
+ * the predicate function passed as the `predicate`
+ * parameter, and then reverse the result. If
+ * `predicate()` returns true, the returned function
+ * returns false, or vice-versa.
+ * @param {function(WeatherRecord)} predicate - an existing
+ * predicate function that will be invoked for each array element
+ * @returns {function(WeatherRecord)} - a new predicate function
+ * that you can pass to the Array.filter() method
+ */
 function not(predicate) {
+    //return a new predicate function that...
     return function(record) {
+        //invokes the function passed as the
+        //predicate parameter, and negates
+        //the result
         return !predicate(record);
     }
 }
 
+/**
+ * Returns the result of record.tempMap >= 60
+ * @param {WeatherRecord} record 
+ * @returns {boolean}
+ */
 function isWarm(record) {
     return record.tempMax >= 60;
 }
 
+//use our isRainy function to filter the WEATHER
+//array to get only the ones where .rain === true
 let rainyDays = WEATHER.filter(isRainy);
 //console.log(rainyDays)
-let dryDays = WEATHER.filter(not(isRainy));
-//can't do this
-//let dryDays = WEATHER.filter(!isRainy);
-//console.log(dryDays);
-let warmDays = WEATHER.filter(isWarm);
-let coldDays = WEATHER.filter(not(isWarm));
 
+//combine not with isRainy to create a new predicate
+//function that filters for records where .rain !== true
+let dryDays = WEATHER.filter(not(isRainy));
+//console.log(dryDays);
+
+//who says that it always rains in Seattle?
 console.log("there were %d rainy days and %d dry days", rainyDays.length, dryDays.length);
+
+/* can't do this--isRainy is a *reference* to the
+function with that name, similar to how a parameter
+is just a reference to the data that was passed
+to that parameter. The expression `isRainy` evaluates
+to a function reference: it does not *invoke* the 
+function. So we can't use ! to reverse the return
+value, as we aren't invoking the function yet.
+*/
+//let dryDays = WEATHER.filter(!isRainy);
+
+//use isWarm to filter for days where tempMax >= 60
+let warmDays = WEATHER.filter(isWarm);
+//again use not(), but this time wrap isWarm to
+//filter for days where tempMax < 60
+let coldDays = WEATHER.filter(not(isWarm));
 
 //SORTING
 /**
@@ -59,8 +108,20 @@ function byTempMax(record1, record2) {
     return record1.tempMax - record2.tempMax;
 }
 
+/**
+ * Returns a new comparator function that will invoke
+ * the function passed as `comparator` and then return
+ * the negation of the result to the caller. This effectively
+ * sorts the array elements in descending rather than 
+ * ascending order.
+ * @param {function(WeatherRecord,WeatherRecord)} comparator 
+ * @returns {function(WeatherRecord,WeatherRecord)}
+ */
 function descending(comparator) {
+    //return a new comparator function that...
     return function(record1, record2) {
+        //invokes the original comparator function
+        //and returns the negation of the result
         return -comparator(record1, record2);
     }
 }
