ef1e6203d3451f03824f093204a5b66def9942b3
Dave Stearns <dlsinfo@uw.edu>
2018-01-23 16:11:46 -0800

    seattle weather data for functional day

--- functional/js/globals.d.ts
+++ functional/js/globals.d.ts
@@ -4,3 +4,12 @@ interface BabyNameRecord {
     count: number;
 }
 declare const BABYNAMES: Array<BabyNameRecord>;
+
+interface WeatherRecord {
+    date: string;
+    precipitation: number,
+    tempMax: number,
+    tempMin: number,
+    rain: boolean
+}
+declare const WEATHER: Array<WeatherRecord>;
