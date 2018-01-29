6aff23c702a9073c8e78448fe6a18079b767edd7
Dave Stearns <dlsinfo@uw.edu>
2018-01-29 11:29:58 -0800

    end of task list demo

--- dom/js/index.js
+++ dom/js/index.js
@@ -22,6 +22,10 @@ let state = {
     ]
 };
 
+let storedJSON = localStorage.getItem("tasks");
+let storedTasks = JSON.parse(storedJSON);
+state.tasks = storedTasks || state.tasks;
+
 /**
  * Renders a single task as an <li>
  * @param {Task} task 
@@ -31,6 +35,18 @@ function renderTask(task) {
     //TODO: create a new <li> for the task,
     //populate it, add a click event listener,
     //and return it
+    let li = document.createElement("li");
+    li.textContent = task.title;
+    li.classList.add("clickable");
+    if (task.done) {
+        li.classList.add("done");
+    }
+    li.addEventListener("click", function() {
+        task.done = !task.done;
+        li.classList.toggle("done");
+        saveTasks(state);
+    });
+    return li;
 }
 
 /**
@@ -41,6 +57,16 @@ function render(state) {
     //TODO: clear the #tasks-ul element
     //and render each task, appending the
     //returned <li> to the <ul>
+    let ul = document.querySelector("#tasks-ul");
+    ul.textContent = "";
+    for (let i = 0; i < state.tasks.length; i++) {
+        ul.appendChild(renderTask(state.tasks[i]));
+    }
+}
+
+function saveTasks(state) {
+    let tasksJSON = JSON.stringify(state.tasks);
+    localStorage.setItem("tasks", tasksJSON);    
 }
 
 //do an initial rendering
@@ -48,8 +74,26 @@ render(state);
 
 //TODO: add event listener for the new task form 
 //submit event that adds new tasks to the list
-
+document.querySelector(".new-task-form")
+    .addEventListener("submit", function(evt) {
+        evt.preventDefault();
+        let titleInput = this.querySelector("input");
+        let title = titleInput.value;
+        let task = {title: title};
+        state.tasks.push(task);
+        saveTasks(state);
+        render(state);
+        titleInput.value = "";
+    });
 
 //TODO: add event listener for the purge completed
 //button click event that purges completed tasks
 //from the list
+document.querySelector(".purge-button")
+    .addEventListener("click", function() {
+        state.tasks = state.tasks.filter(function(task) {
+            return !task.done;
+        });
+        saveTasks(state);
+        render(state);
+    });
