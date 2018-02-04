561899afac86772d9c2a9a856c5240bf72e79a71
Dave Stearns <dlsinfo@uw.edu>
2018-02-04 12:06:10 -0800

    added sample markup to comments

--- es6/src/ProgressBar.js
+++ es6/src/ProgressBar.js
@@ -3,3 +3,6 @@
 //progress bar with a width matching the value
 //passed to the class constructor
 //https://getbootstrap.com/docs/4.0/components/progress/
+//<div class="progress">
+//  <div class="progress-bar progress-bar-striped progress-bar-animated"></div>
+//</div>
