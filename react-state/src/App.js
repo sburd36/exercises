416f9197c97e3f3bbc24e7d1539e84bddeb47590
Dave Stearns <dlsinfo@uw.edu>
2018-02-12 11:27:34 -0800

    react stateful

--- react-state/src/App.js
+++ react-state/src/App.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import * as itunes from "./itunes";
+import TrackCard from "./TrackCard";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            q: "",
+            tracks: []
+        }
+    }
+    handleSubmit(evt) {
+        evt.preventDefault();
+        console.log("searching for %s", this.state.q);
+        fetch(itunes.SEARCH_API + this.state.q)
+            .then(itunes.handleResponse)
+            .then(data => this.setState({
+                tracks: data.results, 
+                fetchError: undefined
+            }))
+            .catch(err => this.setState({fetchError: err}));
+    }
     render() {
         return (
             <div>
@@ -13,18 +33,27 @@ class App extends Component {
                 <main>
                     <section>
                         <div className="container">
-                            <form>
+                            <form onSubmit={evt => this.handleSubmit(evt)}>
                                 <input type="text"
                                     className="form-control"
                                     placeholder="what do you want to listen to?"
+                                    value={this.state.q}
+                                    onInput={evt => this.setState({q: evt.target.value})}
                                     />
                             </form>
                         </div>
                     </section>
                     <section>
                         <div className="container">
+                            {
+                                this.state.fetchError ? 
+                                    <div className="alert alert-danger">{this.state.fetchError.message}</div> :
+                                    undefined
+                            }
                             <div className="results">
-                                TODO: handle search and render results
+                               {
+                                   this.state.tracks.map(t => <TrackCard key={t.trackId} track={t} />)
+                               }
                             </div>
                         </div>
                     </section>
--- react-state/src/TrackCard.js
+++ react-state/src/TrackCard.js
@@ -3,4 +3,17 @@
 // .trackName = track name
 // .artworkUrl100 = URL to track artwork
 // .previewUrl = URL to audio preview
+import React from "react";
 
+export default class TrackCard extends React.Component {
+    render() {
+        return (
+            <figure>
+                <img src={this.props.track.artworkUrl100} alt={this.props.track.trackName} />
+                <figcaption>
+                    {this.props.track.trackName}
+                </figcaption>
+            </figure>
+        );
+    }
+}
