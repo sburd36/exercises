332bd7c3942b3860f7959313e2576bb0f298c8bf
Dave Stearns <dlsinfo@uw.edu>
2018-02-18 11:45:50 -0800

    starter files for firebase database data

---
+++ firebase-data/src/Task.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+//styles for all tasks
+let baseStyles = {
+    cursor: "pointer"
+};
+
+//styles for tasks where `.done` is truthy
+let doneStyles = {
+    //the ... is the spread operator and in this case
+    //it assigns all properties from `baseStyles` to
+    //this new object as well
+    ...baseStyles,
+    textDecoration: "line-through",
+    color: "#AAA"
+}
+
+export default class Task extends React.Component {
+    /**
+     * 
+     * @param {boolean} curDone the current .done state of the task
+     */
+    handleClick(curDone) {
+        //TODO: update the `done` property of the task;
+        //updates must be done through the ref,
+        //but remember that you can get the ref for
+        //a snapshot by accessing the snapshot's .ref
+        //property
+
+    }
+    render() {
+        //TODO: get the task properties and render accordingly;
+        //use `doneStyles` if the task's .done property is truthy,
+        //else use `baseStyles`
+        let styles = doneStyles;
+        return (
+            <li style={styles} onClick={() => this.handleClick()} >
+                TODO: put task title here
+            </li>
+        );
+    }
+}
